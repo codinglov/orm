82dc902ab654d1bba424085b7c9112d4fe662c89
codinglov <112770238+codinglov@users.noreply.github.com>
2023-02-26 12:50:41 -0600

    origin

---
+++ .history/Origin/Develop/models/Tag_20230226124713.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    // define columns
+    id: {
+      type: D
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
